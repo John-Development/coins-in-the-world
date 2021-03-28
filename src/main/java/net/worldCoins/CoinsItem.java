54b129e4691bdcf5f3eb417b4eac1eccb742fdcd
John-Development <38006800+John-Development@users.noreply.github.com>
2021-03-28 15:22:15 +0200

    Added gold and iron coins

---
+++ src/main/java/net/worldCoins/CoinsItem.java
@@ -0,0 +1,9 @@
+package net.worldCoins;
+
+import net.minecraft.item.Item;
+
+public class CoinsItem extends Item {
+  public CoinsItem(Settings settings) {
+    super(settings);
+  }
+}
--- src/main/java/net/worldCoins/worldCoins.java
+++ src/main/java/net/worldCoins/worldCoins.java
@@ -1,10 +1,19 @@
 package net.worldCoins;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class worldCoins implements ModInitializer {
+  public static final Item GOLD_COIN = new CoinsItem(new FabricItemSettings().group(ItemGroup.MISC));
+  public static final Item IRON_COIN = new CoinsItem(new FabricItemSettings().group(ItemGroup.MISC));
+
   @Override
   public void onInitialize() {
-    
+    Registry.register(Registry.ITEM, new Identifier("world-coins", "gold_coin"), GOLD_COIN);
+    Registry.register(Registry.ITEM, new Identifier("world-coins", "iron_coin"), IRON_COIN);
   }
 }
